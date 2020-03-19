738c2d1a0a82681c40ff87f18d262c7ee5d354a8
Diego Muñoz Jorquera <munoz.jorquera.diego@alumnat.copernic.cat>
2020-03-19 14:10:06 +0100

    SDL test 3 collitions

--- Project-COVID-27/Main.cpp
+++ Project-COVID-27/Main.cpp
@@ -23,28 +23,37 @@ typedef struct {
 	SDL_Rect blt;
 } Bullet;
 
-bool isOut(Bullet* a) { return a == NULL; }
+bool isOut(Bullet* a) { return a == nullptr; }
 
-void setSpeed(int& dx, int& dy, const int &DIAGONAL) {
-	float vel = SPEED;
-
+void setSpeed(int& dx, int& dy, const int &VEL, const int &DIAGONAL) {
 	if (dx != 0 && dy != 0) {
 		dx = dx > 0 ? DIAGONAL : DIAGONAL * -1;
 		dy = dy > 0 ? DIAGONAL : DIAGONAL * -1;
 	}
 	else {
 		if (dx != 0)
-			dx = dx > 0 ? vel : -vel;
+			dx = dx > 0 ? VEL : -VEL;
 		if (dy != 0)
-			dy = dy > 0 ? vel : -vel;
+			dy = dy > 0 ? VEL : -VEL;
 	}
 	//std::cout << "dx: " << dx << " dy: " << dy << std::endl;
 	//std::cout << "vel: " << (int)sqrt((dx*dx) + (dy*dy)) << std::endl;
 }
 
+int getCenterX(const SDL_Rect &r) {
+	return (r.x + r.w / 2);
+}
+
+int getCenterY(const SDL_Rect &r) {
+	return (r.y + r.h / 2);
+}
+
+bool areTouching(const SDL_Rect& a, const SDL_Rect& b) {
+	return !((a.y + a.h <= b.y) || (a.y >= b.y + b.h) || (a.x + a.w <= b.x) || (a.x >= b.x + b.w));
+}
+
 void manageEvents(int &dx, int &dy, bool &fire, int &ready, bool &bucle) {
 	int vel = SPEED;
-
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
 		switch (event.type) {
@@ -100,7 +109,7 @@ void manageEvents(int &dx, int &dy, bool &fire, int &ready, bool &bucle) {
 	}
 }
 
-void draw(SDL_Rect &r, std::list<Bullet*> &bullets, SDL_Surface* screenSurface, SDL_Window* window) {
+void draw(SDL_Rect &r, SDL_Rect &f, std::list<Bullet*> &bullets, SDL_Surface* screenSurface) {
 	//background
 	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x3B, 0x70, 0xFF));
 	
@@ -110,11 +119,10 @@ void draw(SDL_Rect &r, std::list<Bullet*> &bullets, SDL_Surface* screenSurface, 
 	}
 
 	//player
-	SDL_FillRect(screenSurface, &r, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
+	SDL_FillRect(screenSurface, &r, SDL_MapRGB(screenSurface->format, 0xFF, areTouching(r,f) ? 0xFF : 0x00, 0x00));
 
-	//update de la pantalla
-	SDL_Delay(20);
-	SDL_UpdateWindowSurface(window);
+	//follower
+	SDL_FillRect(screenSurface, &f, SDL_MapRGB(screenSurface->format, 0xFF, 0x88, 0x00));
 }
 
 int main(int argc, char* argv[]) {
@@ -130,23 +138,63 @@ int main(int argc, char* argv[]) {
 	bool fire = false;
 	int ready = 0;
 
+	int fdx = 0;
+	int fdy = 0;
+
 	int vel = SPEED;
+	const int vel_f = vel * 0.6f;
 	int t_wait = SPACE_BULLETS;
 	const int DIAGONAL = ceil(vel / sqrt(2));
+	const int FOLLOWER_D = ceil((vel_f) / sqrt(2));
 
 	std::list<Bullet*> bullets;
-	Bullet* blt_aux;
 
 	SDL_Window* window = SDL_CreateWindow("COVID_27", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, x, y, SDL_WINDOW_RESIZABLE);
 	
-	SDL_Rect r = { 240, 240, 100, 100 };
+	SDL_Rect r = { x / 2 - 50, y / 2 - 50, 100, 100 };
+	SDL_Rect f = { 0, 0, 50, 50 };
 
 	while (bucle) {
 		SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
 		
 		//input del usuario + set de acciones
 		manageEvents(dx, dy, fire, ready, bucle);
-		setSpeed(dx, dy, DIAGONAL);
+		setSpeed(dx, dy, vel, DIAGONAL);
+
+		if (getCenterX(f) < getCenterX(r)) {
+			if (getCenterX(r) -getCenterX(f) >= vel_f)
+				fdx = vel_f;
+			else
+				fdx = 0;
+		} else {
+			if (getCenterX(f) > getCenterX(r)) {
+				if (getCenterX(f) - getCenterX(r) >= vel_f)
+					fdx = -vel_f;
+				else
+					fdx = 0;
+			} else {
+				fdx = 0;
+			}
+		}
+		if (getCenterY(f) < getCenterY(r)) {
+			if (getCenterY(r) - getCenterY(f) >= vel_f)
+				fdy = vel_f;
+			else
+				fdy = 0;
+		}
+		else {
+			if (getCenterY(f) > getCenterY(r)) {
+				if (getCenterY(f) - getCenterY(r) >= vel_f)
+					fdy = -vel_f;
+				else
+					fdy = 0;
+			}
+			else {
+				fdy = 0;
+			}
+		}
+		setSpeed(fdx, fdy, vel_f, FOLLOWER_D);
+		//std::cout << areTouching(r, f) << " r: " << r.x << ";" << r.y << " f: " << f.x << ";" << f.y << std::endl;
 
 		//gestion del player
 		if (r.x > 0) {
@@ -169,9 +217,9 @@ int main(int argc, char* argv[]) {
 		}
 
 		ready--;
-		if (fire && ready < 0) {
+		if (fire && ready < 0 && !areTouching(f,r)) { //disparos deshabilitados si el follower te esta tocando
 			ready = t_wait;
-			blt_aux = new Bullet;
+			Bullet* blt_aux = new Bullet;
 			blt_aux->blt = { r.x + r.w / 2 - 5, r.y + r.h / 2 - 5, 10, 10 }; //tamanos de bala hardcodeados
 
 			//caso en que este quieto el usuario
@@ -185,7 +233,13 @@ int main(int argc, char* argv[]) {
 			}
 			blt_aux->out = false;
 			bullets.push_back(blt_aux);
+
+			blt_aux = nullptr;
 		}
+
+		//follower
+		f.x += fdx;
+		f.y += fdy;
 
 		//gestion de cada bala
 		for (std::list<Bullet*>::iterator it = bullets.begin(); it != bullets.end(); it++) {
@@ -200,13 +254,34 @@ int main(int argc, char* argv[]) {
 
 			if (aux->y > 0 - aux->h && aux->y < y) {
 				aux->y += (*it)->by;
+				if (areTouching((*aux), f)) { 
+					switch (rand() % 4) {
+					case 0:
+						f.x = 0;
+						f.y = 0;
+						break;
+					case 1:
+						f.x = x - f.w;
+						f.y = 0;
+						break;
+					case 2:
+						f.x = 0;
+						f.y = y - f.h;
+						break;
+					case 3:
+						f.x = x - f.w;
+						f.y = y - f.h;
+						break;
+					}
+					(*it)->out = true; //si impacta el proyectil desaparece
+				}
 			} else {
 				(*it)->out = true;
 			}
 
 			if ((*it)->out) {
 				delete (*it);
-				(*it) = NULL;
+				(*it) = nullptr;
 			}
 		}
 
@@ -215,7 +290,11 @@ int main(int argc, char* argv[]) {
 		//std::cout << "bullets: " << bullets.size() << std::endl;
 
 		//mostramos el frame
-		draw(r, bullets, screenSurface, window);
+		draw(r, f, bullets, screenSurface);
+
+		//update de la pantalla
+		SDL_Delay(15);
+		SDL_UpdateWindowSurface(window);
 	}
 
 	SDL_Quit();
